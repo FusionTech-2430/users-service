e8782936049fe62732291fc0fb785a4b9157c38b
Estebans441 <salazaraesteban@javeriana.edu.co>
2024-09-12 22:58:31 -0500

    🥅 add error catching

--- src/main/java/co/allconnected/fussiontech/usersservice/controllers/UsersController.java
+++ src/main/java/co/allconnected/fussiontech/usersservice/controllers/UsersController.java
@@ -1,10 +1,12 @@
 package co.allconnected.fussiontech.usersservice.controllers;
 
+import co.allconnected.fussiontech.usersservice.dtos.ErrorResponse;
 import co.allconnected.fussiontech.usersservice.dtos.UserCreateDTO;
 import co.allconnected.fussiontech.usersservice.dtos.UserDTO;
 import co.allconnected.fussiontech.usersservice.services.UserService;
 import com.google.firebase.auth.FirebaseAuthException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -23,9 +25,19 @@ public class UsersController {
     }
 
     @PostMapping
-    public ResponseEntity<UserDTO> createUser(@ModelAttribute UserCreateDTO user,
-                                              @RequestParam(value = "photo", required = false) MultipartFile photo)
-            throws IOException, FirebaseAuthException {
-        return ResponseEntity.ok(new UserDTO(userService.createUser(user, photo)));
+    public ResponseEntity<?> createUser(
+            @ModelAttribute UserCreateDTO user,
+            @RequestParam(value = "photo", required = false) MultipartFile photo) {
+        try{
+            UserDTO userDTO = new UserDTO(userService.createUser(user, photo));
+            return ResponseEntity.status(HttpStatus.CREATED).body(userDTO);
+        }
+        catch (FirebaseAuthException e) {
+            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Firebase authentication error: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
+        } catch (Exception e) {
+            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Unexpected error occurred: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+        }
     }
 }
