79294e6e7711539647239d5b34e90fb27034258f
Estebans441 <salazaraesteban@javeriana.edu.co>
2024-10-08 15:52:06 -0500

    :lock: update cors

--- src/main/java/co/allconnected/fussiontech/usersservice/config/WebConfig.java
+++ src/main/java/co/allconnected/fussiontech/usersservice/config/WebConfig.java
@@ -12,8 +12,9 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .exposedHeaders("Content-Type", "Authorization")
+                .allowedHeaders("Content-Type", "Authorization", "Accept", "X-Requested-With", "Access-Control-Allow-Headers", "Origin", "multipart/form-data") // Asegúrate de permitir 'multipart/form-data'
+                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
                 .allowCredentials(true);
     }
+
 }
